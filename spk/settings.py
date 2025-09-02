a9d17d907762c731cdb7c1d757c48fca45337740
aditiaputra100 <aditia.putra100@gmail.com>
2025-09-02 23:38:20 +0700

    fix: set up ssh & static files

--- spk/settings.py
+++ spk/settings.py
@@ -92,12 +92,6 @@ WSGI_APPLICATION = 'spk.wsgi.application'
 #     }
 # }
 
-print(f"ENGINE: {os.getenv('DATABASE_ENGINE')}")
-print(f"NAME: {os.environ.get('DATABASE_NAME')}")
-print(f"USERNAME: {os.environ.get('DATABASE_USERNAME')}")
-print(f"HOST: {os.environ.get('DATABASE_HOST')}")
-print(f"PORT: {os.environ.get('DATABASE_PORT')}")
-
 DATABASES = {
     'default': {
         'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.mysql'),
@@ -145,6 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
